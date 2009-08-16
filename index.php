8a10b1849f51475e3fb330ff182c6fc2a632e076
Sirupsen <sirup@sirupsen.dk>
2009-08-16 16:47:16 +0200

    Removed the content, rewriting it with style..

--- index.php
+++ index.php
@@ -16,33 +16,7 @@ $test_class = new TestClass;
 // Instances the Template class
 $tpl = new Template;
 
-// We simply set the variable "name" ($name), to "Sirupsen"
-// So: <?php echo $sirupsen; ?/> in our template file would
-// simply output: "Sirupsen"
-$tpl->name = 'Sirupsen';
 
-// This is a simple preview of how the template system works
-// with an array, this is just a manually placed one.
-$tpl->articles = array(array('title' => 'Title #1', 'content' => 'Content #1'), array('title' => 'Title #2', 'content' => 'Content #2')); 
 
-// Now we use our select_tpl function to simply put in the
-// rows from the query into the names variable ($names)
-$tpl->names = $registry->db->select_tpl("SELECT * FROM names");
-
-$data = array(
-	'name' => 'name_raw',
-	'age' => 'column_raw'
-);
-
-$registry->db->insert('names', $data);
-
-// This time we use a method from the TestClass, and puts
-// it into the fucked variable ($fucked), this method
-// simply takes some stuff from a database and makes
-// some weird manipulations (hence the name, fucked)
-$tpl->fucked = $test_class->selectCustom();
-
-// This outputs the template file which in this case, as 
-// this file is called index.php, would be templates/index.php
 echo $tpl;
 ?>
