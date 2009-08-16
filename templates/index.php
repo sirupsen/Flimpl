bdf85055e04e2bfda30839a2390844fbf4d407b9
Sirupsen <sirup@sirupsen.dk>
2009-08-16 17:04:32 +0200

    Added some styled text

---
+++ templates/index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title><?php echo $title; ?></title>
+	<link rel="stylesheet" href="static/css/style.css" type="text/css">
+</head>
+
+<body>
+	<div id="wrapper">
+		<div id="top">
+			<h1>Flimpl</h1>
+			<div class="indent">
+				<h2>Framework, Simple</h2>
+			</div>
+		</div>
+
+		<div id="content">
+			<h3>This is a sample file, it's here in order to welcome you, and check if everything is working correctly, also you can check the source in order to get started..<h3>
+		</div>
+	</div>
+</body>
+</html>
