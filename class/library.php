5890ec2218e2dbeceafdb0af55a81ca0add69b3f
Sirupsen <sirup@sirupsen.dk>
2009-08-20 19:06:16 +0200

    Added a tweet method, tweet away!

--- class/library.php
+++ class/library.php
@@ -58,5 +58,20 @@ class Library {
 		
 		return "$difference $periods[$j] {$tense}";
 	}
+
+	public function tweet($message, $user, $password) {
+		$context = stream_context_create(array( 
+			'http' => array( 
+			  'method'  => 'POST', 
+			  'header'  => sprintf("Authorization: Basic %s\r\n", base64_encode($username.':'.$password)). 
+						   "Content-type: application/x-www-form-urlencoded\r\n", 
+			  'content' => http_build_query(array('status' => $message)), 
+			  'timeout' => 5, 
+			), 
+		  )); 
+		  $ret = file_get_contents('http://twitter.com/statuses/update.xml', false, $context); 
+		  
+		  return false !== $ret; 
+	}
 }
 ?>
