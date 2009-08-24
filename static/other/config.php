888740a86cf619e3ee30138c117755226871ea66
Sirupsen <sirup@sirupsen.dk>
2009-08-24 20:34:48 +0200

    Debug stuff

--- static/other/config.php
+++ static/other/config.php
@@ -1,6 +1,11 @@
 <?php
 // Configuration
 
+// Are we debugging? Showing errors directly, instead
+// of logging them. If it's set to false, it's going to
+// put errors in the errors table.
+$config['debug'] = TRUE;
+
 // Database Configuration
 $config['db_host'] = 'localhost';
 $config['db_user'] = 'root';
