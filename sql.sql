867c16068e2dd9d776c258cb3187ac316e05be5c
Sirupsen <sirup@sirupsen.dk>
2009-08-24 20:57:47 +0200

    Updated SQL file

--- sql.sql
+++ sql.sql
@@ -1,6 +1,6 @@
 --
 -- MySQL 5.1.33
--- Sun, 16 Aug 2009 15:47:01 +0000
+-- Mon, 24 Aug 2009 18:56:36 +0000
 --
 
 CREATE DATABASE `db` DEFAULT CHARSET latin1;
@@ -19,6 +19,19 @@ CREATE TABLE `articles` (
 INSERT INTO `articles` (`id`, `title`, `text`, `author`, `time`) VALUES 
 ('1', 'Article Post #1', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.<br/><br/>\n\nDonec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.', 'Sirupsen', '2009-08-16 16:51:20'),
 ('2', 'Article Post #2', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.<br/><br/>\n\nDonec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.', 'Sirupsen', '2009-08-16 16:51:20');
+
+CREATE TABLE `errors` (
+   `id` int(11),
+   `no` varchar(255),
+   `message` text,
+   `file` varchar(255),
+   `line` varchar(255),
+   `created` timestamp,
+   `time` int(11),
+   PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET latin1;
+
+-- [Table `errors` is empty]
 
 CREATE TABLE `links` (
    `id` int(11),
