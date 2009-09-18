94584a4df4806595f28e73bbb305fb28794e0fa4
Sirupsen <sirup@sirupsen.dk>
2009-09-18 13:37:07 +0200

    Added a folder to add included folders to

---
+++ include/pages/top.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title><?php echo $title; ?></title>
+	<link rel="stylesheet" href="include/css/style.css" type="text/css">
+</head>
+<body>
+	<div id="wrapper">
+		<div id="top">
+			<h1>Flimpl</h1>
+			<div class="indent">
+				<h2>Framework Simple</h2>
+			</div>
+		</div>
+		<div id="content">
