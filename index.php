199fab196b0e20440dfb18ac8945883ca42172e5
Sirupsen <sirup@sirupsen.dk>
2009-09-23 19:16:29 +0200

    Rewrite in order to use some smart rewriting"

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
-require_once("bootstrap.php");
+$page = $_GET['page'] . '.php';
 
-// Create instance of template [required]
-// Top is included right here
-$tpl = new Template;
-
-// Assign variables
-$tpl->title = 'Welcome to Flimpl!';
-
-// Write out the template
-// Bottom is included afterwards
-echo $tpl;
+if(file_exists($page)) {
+	require_once($page);
+} elseif (!$_GET['page']) {
+	require_once("home.php");
+} else {
+	require_once("404.php");
+}
