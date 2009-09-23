62ebd973eebfa7498eec81ae61576c37043d3af5
Sirupsen <sirup@sirupsen.dk>
2009-09-23 19:16:49 +0200

    Initial commit of new "index" file

---
+++ home.php
@@ -0,0 +1,12 @@
+<?php
+require_once("bootstrap.php");
+// Create instance of template [required]
+// Top is included right here
+$tpl = new Template;
+
+// Assign variables
+$tpl->title = 'Welcome to Flimpl!';
+
+// Write out the template
+// Bottom is included afterwards
+echo $tpl;
