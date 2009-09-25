e2717c52042b4f2ab36eac43c49b19ab9ecdcae4
Sirupsen <sirup@sirupsen.dk>
2009-09-25 21:00:00 +0200

    Cleanup and optimization

--- index.php
+++ index.php
@@ -1,22 +1,20 @@
 <?php
 $page = $_GET['page'];
 
+// Create instance of template [required]
+// Top is included right here
+$tpl = new Template;
+
 // If the file requested exists, write out the content
 if (file_exists($page . '.php')) {
-	// Create instance of template [required]
-	// Top is included right here
-	$tpl = new Template;
 	require_once($page);
 
-	// If it's not set, include the home file
+// If it's not set, include the home file
 } elseif (!$page) {
-	// Create instance of template [required]
-	// Top is included right here
-	$tpl = new Template;
 	require_once("home.php");
 
-	// If the file doesn't exist, and it's set but that page doesn't exist
-	// Redirect to 404
+// If the file doesn't exist, and it's set but that page doesn't exist
+// Redirect to 404
 } else {
 	header("404.php");
 }
