d2a77ec7a4a32deb12e6bcee283e0ea0c3f6cf84
Sirupsen <sirup@sirupsen.dk>
2009-09-25 21:21:56 +0200

    Various changes

--- home.php
+++ home.php
@@ -1,4 +1,8 @@
 <?php
+require('bootstrap.php');
+
+$tpl = new Template;
+
 // Assign variables
 $tpl->title = 'Welcome to Flimpl!';
 
--- index.php
+++ index.php
@@ -1,21 +1,20 @@
 <?php
-require('bootstrap.php');
 $page = $_GET['page'];
 
 // Create instance of template [required]
 // Top is included right here
-$tpl = new Template;
 
 // If the file requested exists, write out the content
 if (file_exists($page . '.php')) {
-	require_once($page);
+	require($page . '.php');
 
 // If it's not set, include the home file
 } elseif (!$page) {
-	require_once("home.php");
+	require("home.php");
 
 // If the file doesn't exist, and it's set but that page doesn't exist
 // Redirect to 404
 } else {
-	header("404.php");
+	echo $page;
+	require("404.php");
 }
