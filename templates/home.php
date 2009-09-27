6c93447be8b1338bf48c9648a5b435499eeb1fcd
Sirupsen <sirup@sirupsen.dk>
2009-09-27 16:35:32 +0200

    Better text

--- templates/home.php
+++ templates/home.php
@@ -1,10 +1,10 @@
-<p>Welcome to the Flimpl framework! A simple framework made available to you by <a href="http://www.sirupsen.dk" title="Go to sirupsen.dk">Sirupsen</a>. To get started, I encourage you to read through <a href="README.markdown" title="Go to readme file">the readme file</a>, to get an overview of how Flimpl works. Also, I hope you'll read through the source code (this should give you the best understanding), it's faily well commented.</p>
+<p>Welcome to the Flimpl framework! A simple framework made available to you by <a href="http://www.sirupsen.dk" title="Go to sirupsen.dk">Sirupsen</a>. To get started, I encourage you to read through <a href="README.markdown" title="Go to readme file">the readme file</a>, to get an overview of how Flimpl works.</p>
 
 <p>If you enjoy working with Flimpl, I'd love <a href="http://sirupsen.dk#contact" title="Go to the contact page at sirupsen.dk">to hear from you!</a>. If you have any ideas, <a href="http://sirupsen.dk#contact" title="Go to the contact page at sirupsen.dk">don't hestiate to contact me.</a> If you make any changes, or improvements please submit the changes to Github.</p>
 
-<p>For the latest version and updates, be sure to have <a href="http://github.com/Sirupsen/Flimpl/" title="Go to the Github repo for Flimpl">the Github repo</a> bookmarked.</p>
+<p>For the latest updates, pull from the <a href="http://github.com/Sirupsen/Flimpl/" title="Go to the Github repo for Flimpl">the Github repo</a>.</p>
 
-<p><a href="#" id="test_database">Click me</a> to test if database is functioning correctly. <a href="#" id="create_errors">Click here</a> if you want to create error logging table. </p>
+<p>Let's go ahead and do some simple testing. <a href="#" id="test_database">Click me</a> to test if the database is functioning correctly. <a href="#" id="create_errors">Click here</a> if you want to create the error logging table. </p>
 
 <p id="database_response" class="msg hidden"></p>
 <p id="error_logging" class="msg hidden"></p>
