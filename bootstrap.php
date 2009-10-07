ae6bd6a443980a426c230d22ea4243ed138c9c60
Sirupsen <sirup@sirupsen.dk>
2009-10-07 22:07:01 +0200

    Error handlers directly in bootstrap file

--- bootstrap.php
+++ bootstrap.php
@@ -17,9 +17,52 @@
 // Require the config file for some global configuration
 require_once('config.php');
 
-// Error handlers
-require_once('include/misc/functions/error_handler.php');
-require_once('include/misc/functions/exception_handler.php');
+// Creates a function to handle errors, and log them.
+// @TODO: Use Mysqli
+function error_handler($errno, $errstr, $errfile, $errline) {
+	// We want to be able to use the configuration.
+	global $config;
+
+	// If we are debugging, don't log stuff. You should
+	// use debugging when you are developping locally, else
+	// it should be disabled for security purposes.
+	if ($config['debug'] == FALSE) {
+		$link = mysql_connect($config['db_host'], $config['db_user'], $config['db_pass']);
+		$db = mysql_select_db($config['db_database'], $link);
+
+		// To be sure the query can be performed
+		$errorstr = mysql_real_escape_string($errstr);
+		$errfile = mysql_real_escape_string($errfile);
+
+		// We don't want to give the user some kind of tricky
+		// and non-prof php-error.
+		echo "An error occured. It've been logged, and will be fixed as soon as possible.";
+
+		// Unix timestamp, useful if your going to create some
+		// kind of custom error platform.
+		$time = time();
+
+		// Make the query
+		$sql = "INSERT INTO errors SET no = '$errorno', message = '$errorstr', file = '$errfile', line = '$errline', time = '$time'";
+
+		// Perform the query
+		mysql_query($sql);
+
+		// Else, we are in debugging mode and we can safely
+		// just print the error.
+	} else {
+		echo $errorstr;
+	}
+}
+
+// Makes a function which can be used to handle exceptions
+function exception_handler($exception) { ?>
+	<div class="error">
+		<?php echo $exception->getMessage(); ?>
+	</div>
+<?php
+}
+
 // Activate them
 set_error_handler('error_handler');
 set_exception_handler('exception_handler');
