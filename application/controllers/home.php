676c002f4369aab090cbbe205cfd2cc85b6eee0b
Sirupsen <sirup@sirupsen.dk>
2009-10-13 17:44:42 +0200

    Documentation for home controller

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ *
+ * The home controller is the controller for the index of the
+ * entire thing, f.e it would be accesible from:
+ * http://flimpl.com/
+ *
+ */
+
 class Home extends Controller {
 	public function index() {
 		$this->template->title = 'Welcome to Flimpl';
