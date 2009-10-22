c5364cc10c9720fdab8fef0b1078af81b14b085d
Sirupsen <sirup@sirupsen.dk>
2009-10-22 22:45:05 +0200

    Moved stuff around, most revelvant stuff first

--- system/config/config.php
+++ system/config/config.php
@@ -1,18 +1,6 @@
 <?php
 /*
  *
- * Dev debug is a mode for advanced debugging, giving you a
- * message each time something loads, making it easier to
- * troubleshoot what might be wrong!
- *
- * @default 	false
- *
- */
-
-$config['dev_debug'] = false;
-
-/*
- *
  * Debug mode is a mode you can turn on when you are working
  * locally, just developping. You want detailed errors to be
  * showed, and not logged.
@@ -28,3 +16,15 @@ $config['db_host'] 		= 'localhost'; // Host? Usually "localhost"
 $config['db_user'] 		= 'root'; // Username? Locally this is usually "root"
 $config['db_pass'] 		= ''; // Password?
 $config['db_database'] 	= 'db'; // Database we're using
+
+/*
+ *
+ * Dev debug is a mode for advanced debugging, giving you a
+ * message each time something loads, making it easier to
+ * troubleshoot what might be wrong!
+ *
+ * @default 	false
+ *
+ */
+
+$config['dev_debug'] = false;
