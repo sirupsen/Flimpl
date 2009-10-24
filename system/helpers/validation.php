898f19004b7669eae57834ee27851ae2ad707cd9
Sirupsen <sirup@sirupsen.dk>
2009-10-25 00:49:56 +0200

    Enhanced commenting

--- system/helpers/validation.php
+++ system/helpers/validation.php
@@ -1,12 +1,16 @@
 <?php
 
 class Validation {
+    // Wielding all errors
 	public $errors = array();
+    // Wielding all rules as well as their arguments
 	public $rules;
 
+    // Wielding the input
 	public $input;
 
 	public function __construct($input) {
+        // Set input
 		$this->input = $input;
 	}
 
@@ -28,20 +32,25 @@ class Validation {
 				$args = str_replace('\,', ',', $args);
 			}
 
+            // Add the rule with it's arguments
 			$this->rules[$field][] = array($rule, $args);
 		}
 	}
 
 	public function validate() {
+        // Foreach of the fields [I.e. Username]
 		foreach ($this->rules as $key => $rules) {
+            // For each of the rules for this field [Length]
 			foreach ($rules as $rule) {
+                // If the function returns false, 
 				if(!call_user_func_array(array('Validators', $rule['0']), array($this->input[$key], $rule['1']))) {
-
+                    // Add the error
 					$this->errors[$rule['0']] = $rule['1'];
 				}
 			}
 		}
 
+        // Return true if there are no errors
 		return $this->errors === array();
 	}
 }
