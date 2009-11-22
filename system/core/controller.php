dfdb658a95447ce07c4fe3b7fe91fdae080b0ed9
Sirupsen <sirup@sirupsen.com>
2009-11-22 22:34:02 +0100

    Comments for the main controller

--- system/core/controller.php
+++ system/core/controller.php
@@ -1,14 +1,54 @@
 <?php
+/*
+ *
+ * Main controller which should be extended by any other
+ * controller in order to supply it with a template, libraries
+ * and such.
+ *
+ */
+
 class Controller {
+	// Template instance
 	protected $template;
+
+	// Database instance
 	protected $db;
+
+	// Input instance
 	protected $input;
 
+	/*
+	 *
+	 * When we instance our controller, load libraries and
+	 * instance the template.
+	 *
+	 * @param    string    $controller    Name of the controller
+	 * @param    string    $method        Name of the method
+	 *
+	 */
+
 	public function __construct($controller, $method) {
+		// Create the template
 		$this->template = new Template($controller, $method);
+
+		// Get the instance of the database
 		$this->db = Database::instance();
+
+		// Create/Get the instance of Input, cleans all $_POST data
 		$this->input = Input::instance();
 	}
+
+	/*
+	 *
+	 * Easy way to supply data for the template, instead of:
+	 *     $this->template->set('name', 'data');
+	 * We do:
+	 *     $this->name = 'data';
+	 *
+	 * @param    string    $name    Name of the value
+	 * @param    mixed     $data    Data
+	 *
+	 */
 
 	public function __set($name, $data) {
 		$this->template->$name = $data;
