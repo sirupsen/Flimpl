7444b82027f5afd5bb214579585fba1c68e73889
Sirupsen <sirup@sirupsen.com>
2009-11-22 22:38:54 +0100

    Comments on html class

--- system/helpers/html.php
+++ system/helpers/html.php
@@ -7,6 +7,25 @@
 
 class Html {
 
+	/*
+	 *
+	 * Provides an easy way to create a link to another item
+	 * on the site, because of the mod_rewriting it gets more
+	 * complicated to do urls like:
+	 *     <a href="post/view/12" />
+	 * We need the full url of the site, and for max portability
+	 * for your application this function will automatically
+	 * figure out the path for your site, and provide the correct
+	 * link, so above example would be:
+	 *     <a href="<?php echo Html::anchor('post/view/12') ?>" />
+	 * Which for the domain flimpl.com with flimpl in root would
+	 * result in:
+	 *     <a href="http://flimpl.com/post/view/12" />
+	 *
+	 * @param    string    $link    The link [I.e. post/view/12]
+	 * @return   string    ...      The real link
+	 *
+	 */
 	public static function anchor($link) {
 		return Url::site() . $link;				
 	}
