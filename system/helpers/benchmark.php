a7c282dd21d29353af1a1da3323a8b4f83a1887e
Sirupsen <sirup@sirupsen.dk>
2009-12-02 16:10:41 +0100

    Comments

--- system/helpers/benchmark.php
+++ system/helpers/benchmark.php
@@ -1,19 +1,55 @@
 <?php
 
+/*
+ *
+ * Benchmarking of the application
+ *
+ */
+
 class Benchmark {
+	// The time when we started tracking
 	public static $start_time;
+
+	// The time when we stopped tracking
 	public static $end_time;
+
+	/*
+	 *
+	 * Start the benchmarking
+	 *
+	 * @param    string    $name    Name of the benchmark
+	 *
+	 */
 
 	public static function start($name = 'default') {
 		self::$start_time[$name] = explode(' ', microtime());
 	}
 
+	/*
+	 *
+	 * Stop the benchmarking
+	 * 
+	 * @param    string    $name   Name of the benchmark
+	 *
+	 */
+
 	public static function end($name = 'default') {
 		self::$end_time[$name] = explode(' ', microtime());
 	}
 
+	/*
+	 *
+	 * What's the result of the benchmark in seconds?
+	 *
+	 * @param    string    $name    Name of the benchmark
+	 * @return   int       ..       The time in seconds
+	 *
+	 */
+
 	public static function result($name = 'default') {
+		// Small first
 		$current = self::$end_time[$name][1] - self::$start_time[$name][1];
+		// Then the big
 		$current += self::$end_time[$name][0] - self::$start_time[$name][0];
 
 		return $current;
