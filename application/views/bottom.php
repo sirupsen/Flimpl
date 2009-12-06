475e56b4ce196cd85d0e81bafa70339b734c0eb4
Sirupsen <sirup@sirupsen.dk>
2009-12-06 10:11:11 +0100

    Comments for the bottom file

--- application/views/bottom.php
+++ application/views/bottom.php
@@ -1,11 +1,13 @@
 		</div>
 	</div>
+<!-- Get jQuery and jQueryUI from Google -->
 <script src="http://www.google.com/jsapi" type="text/javascript"></script> 
 <script type="text/javascript"> 
 	google.load("jquery", "1.3.2");
 	google.load("jqueryui", "1.7.1");
 </script>
 
-<script type="text/javascript" src="<?php echo Html::anchor('js/scripts'); ?>"></script>
+<!-- Include the JS script -->
+<script type="text/javascript" src="<?php echo html::anchor('js/scripts'); ?>"></script>
 </body>
 </html>
