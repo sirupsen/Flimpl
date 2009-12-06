dc352664d9b79a9fe4d083d84667282496ae96d6
Sirupsen <sirup@sirupsen.dk>
2009-12-06 15:34:35 +0100

    Added Flimpl python handler

--- flimpl.py
+++ flimpl.py
@@ -1,41 +1,45 @@
 import sys
 import os
 
-if sys.argv[1] == 'app':
-	controller = open('application/controllers/' + sys.argv[2] + '.php', 'wa')
-	controller.write('<?php\n\nclass ' + sys.argv[2] + '_Controller extends Controller {\n\n}')
-	controller.close()
+class Flimpl:
 
-	model = open('application/models/' + sys.argv[2] + '.php', 'wa')
-	model.write('<?php\n\nclass ' + sys.argv[2] + '_Model {\n\n}')
-	model.close()
+	def app():
+		controller = open('application/controllers/' + sys.argv[2] + '.php', 'wa')
+		controller.write('<?php\n\nclass ' + sys.argv[2] + '_Controller extends Controller {\n\n}')
+		controller.close()
 
-	os.mkdir('application/views/' + sys.argv[2])
+		model = open('application/models/' + sys.argv[2] + '.php', 'wa')
+		model.write('<?php\n\nclass ' + sys.argv[2] + '_Model {\n\n}')
+		model.close()
 
-	index = open('application/views/' + sys.argv[2] + '.php', 'wa')
-	index.close()
+		os.mkdir('application/views/' + sys.argv[2])
 
-if sys.argv[1] == 'appdel':
-	input = raw_input("Are you sure? ")
+		index = open('application/views/' + sys.argv[2] + '.php', 'wa')
+		index.close()
 
-	if input in ('y', 'Y', 'yes'):
-		if os.path.exists('application/controllers/' + sys.argv[2] + '.php'):
-			os.remove('application/controllers/' + sys.argv[2] + '.php')
-			print "Controller for", sys.argv[2], "app deleted"
+	def appdel():
+		input = raw_input("Are you sure? ")
+
+		if input in ('y', 'Y', 'yes'):
+			if os.path.exists('application/controllers/' + sys.argv[2] + '.php'):
+				os.remove('application/controllers/' + sys.argv[2] + '.php')
+				print "Controller for", sys.argv[2], "app deleted"
+			else:
+				print "Controller for", sys.argv[2], "app not found"
+
+			if os.path.exists('application/models/' + sys.argv[2] + '.php'):
+				os.remove('application/models/' + sys.argv[2] + '.php')
+				print "View for", sys.argv[2], "app deleted"
+			else:
+				print "Model for", sys.argv[2], "app not found"
+			
+			if os.path.exists('application/views/' + sys.argv[2]):
+				os.removedirs('application/views/' + sys.argv[2])
+				print "View for", sys.argv[2], "app deleted"
+			else:
+				print "View for", sys.argv[2], "app not found"
 		else:
-			print "Controller for", sys.argv[2], "app not found"
+			print "Not deleting app", sys.argv[2]
 
-		if os.path.exists('application/models/' + sys.argv[2] + '.php'):
-			os.remove('application/models/' + sys.argv[2] + '.php')
-			print "View for", sys.argv[2], "app deleted"
-		else:
-			print "Model for", sys.argv[2], "app not found"
-		
-		if os.path.exists('application/views/' + sys.argv[2]):
-			os.removedirs('application/views/' + sys.argv[2])
-			print "View for", sys.argv[2], "app deleted"
-		else:
-			print "View for", sys.argv[2], "app not found"
-	else:
-		print "Not deleting app", sys.argv[2]
 
+Flimpl.sys.argv[1]()
