e2ab1454ac78655eb3296e63575a23e513f19ec9
Sirupsen <sirup@sirupsen.dk>
2010-02-13 22:37:16 +0100

    More text, removed some javascript

--- application/views/bottom.php
+++ application/views/bottom.php
@@ -1,13 +1,4 @@
 		</div>
 	</div>
-<!-- Get jQuery and jQueryUI from Google -->
-<script src="http://www.google.com/jsapi" type="text/javascript"></script> 
-<script type="text/javascript"> 
-	google.load("jquery", "1.3.2");
-	google.load("jqueryui", "1.7.1");
-</script>
-
-<!-- Include the JS script -->
-<script type="text/javascript" src="<?php echo html::a('js/scripts'); ?>"></script>
 </body>
 </html>
